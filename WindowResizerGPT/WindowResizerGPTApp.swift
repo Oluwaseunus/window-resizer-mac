1b3158472225400dcfea197d2da2c2a2be04ac03
Oluwaseun <oluwaseunus@gmail.com>
2024-12-07 20:23:01 +0100

    Initial Commit

---
+++ WindowResizerGPT/WindowResizerGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowResizerGPTApp.swift
+//  WindowResizerGPT
+//
+//  Created by Oluwaseun Adetunji on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WindowResizerGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
